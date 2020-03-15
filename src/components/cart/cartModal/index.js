a3d2374c8dc69f4943a615ade4128fc80bd8e668
Riloko <nikolayko96@gmail.com>
2020-03-15 20:53:22 +0300

    some fixes

--- src/components/cart/cartModal/index.js
+++ src/components/cart/cartModal/index.js
@@ -4,70 +4,10 @@ import CartModalItem from './cartModalItem';
 
 import './cartModal.sass'
 
-class CartModal extends Component {
-
-    state = {
-        items:[
-            {
-                "img": "https://storage.vsemayki.ru/images/0/1/1960/1960261/previews/people_8_child_tshirt_front_white_250.jpg",
-                "text": "BRAWL STARS LEON SHARK",
-                "type": "Детская футболка 3D",
-                "price": "1535 руб.",
-                "popular": false,
-                "banner": false,
-                "sale": false,
-                "count": "1"
-            },{
-                "img": "https://storage.vsemayki.ru/images/0/1/1852/1852825/previews/people_13_manshortfull_front_white_250.jpg",
-                "text": "Monsters Rick and Morty",
-                "type": "Мужская футболка 3D",
-                "price": "1660 руб.",
-                "popular": false,
-                "banner": false,
-                "sale": true,
-                "count": "3"
-            }
-        ]
-    }
-
-    addItemHandle = (id) => {
-        const arr = this.state.items;
-        arr[id].count = parseInt(arr[id].count)+1;        
-        this.setState({          
-            items:arr
-        })
-    }
-
-    removeItemHandle = (id) => {
-        const arr = this.state.items;        
-        if(parseInt(arr[id].count) != 1){
-            arr[id].count = parseInt(arr[id].count)-1;
-            this.setState({          
-                items:arr
-            })
-        }
-        return null
-    }
-
-    fullCount = (arr) => {
-        let fullCount = 0;
-        for (let i = 0; i < arr.length; i++){
-            fullCount += parseInt(arr[i].count);
-        }
-        return fullCount;
-    }
-
-    fullSum = (arr) => {
-        let fullSum = 0;
-        for (let i = 0; i < arr.length; i++){
-            fullSum += arr[i].sale ? ((parseInt(arr[i].price)-(parseInt(arr[i].price)*0.2))*arr[i].count) : parseInt(arr[i].price)*arr[i].count;            
-        }
-        return fullSum;
-    }
-
-
+class CartModal extends Component { 
     render() {
-        const {items} = this.state;
+        const {items, onAddItem, onRemoveItem, FullCount, FullSum} = this.props;
+        console.log(items)
         return (
             <div className="cart-modal">
                 <div className="cart-modal-content">
@@ -80,9 +20,9 @@ class CartModal extends Component {
                             items.map((item, index) => {
                                 return(
                                     <CartModalItem
-                                    {...this.state.items[index]}
-                                    onAddItem={this.addItemHandle}
-                                    onRemoveItem={this.removeItemHandle}                                    
+                                    {...this.props.items[index]}
+                                    onAddItem={onAddItem}
+                                    onRemoveItem={onRemoveItem}                                    
                                     id={index}/>
                                 )
                             })
@@ -94,12 +34,12 @@ class CartModal extends Component {
                             <span className="cart-modal-content-footer-controls-items">
                                 Всего вещей: 
                                 <span className="cart-modal-content-footer-controls-items-count">
-                                    {this.fullCount(items)}
+                                    {FullCount(items)}
                                 </span>
                             </span>
                             <span className="cart-modal-content-footer-controls-sum">
                                 Сумма: 
-                                <span className="cart-modal-content-footer-controls-sum-amount">{this.fullSum(items)} USD</span>
+                                <span className="cart-modal-content-footer-controls-sum-amount">{FullSum(items)} USD</span>
                             </span>
                         </div>
                     </div>
--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -5,13 +5,89 @@ import CartModal from './cartModal/';
 import './cart.sass'
 
 class Cart extends Component {
+
+    state = {
+        items:[
+            {
+                "img": "https://storage.vsemayki.ru/images/0/1/1960/1960261/previews/people_8_child_tshirt_front_white_250.jpg",
+                "text": "BRAWL STARS LEON SHARK",
+                "type": "Детская футболка 3D",
+                "price": "1535 руб.",
+                "popular": false,
+                "banner": false,
+                "sale": false,
+                "count": "1"
+            },{
+                "img": "https://storage.vsemayki.ru/images/0/1/1852/1852825/previews/people_13_manshortfull_front_white_250.jpg",
+                "text": "Monsters Rick and Morty",
+                "type": "Мужская футболка 3D",
+                "price": "1660 руб.",
+                "popular": false,
+                "banner": false,
+                "sale": true,
+                "count": "3"
+            }
+        ]
+    }
+
+    addItemHandle = (id) => {
+        const arr = this.state.items;
+        arr[id].count = parseInt(arr[id].count)+1;        
+        this.setState({          
+            items:arr
+        })
+    }
+
+    removeItemHandle = (id) => {
+        const arr = this.state.items;        
+        if(parseInt(arr[id].count) != 1){
+            arr[id].count = parseInt(arr[id].count)-1;
+            this.setState({          
+                items:arr
+            })
+        }
+        return null
+    }
+
+    fullCount = (arr) => {
+        if(arr){
+            let fullCount = 0;
+        for (let i = 0; i < arr.length; i++){
+            fullCount += parseInt(arr[i].count);
+        }
+        return fullCount;
+        }
+        return null
+    }
+
+    fullSum = (arr) => {
+        if (arr) {
+            let fullSum = 0;
+        for (let i = 0; i < arr.length; i++){
+            fullSum += arr[i].sale ? ((parseInt(arr[i].price)-(parseInt(arr[i].price)*0.2))*arr[i].count) : parseInt(arr[i].price)*arr[i].count;            
+        }
+        return fullSum;
+        }
+        return null
+    }
+
     render() {
+    
+        const {items} = this.state;
         const {img} = this.props;
+        const headerCounter = items ? <span className="header-navbar-controls__item-counter">{this.fullCount(items)}</span> : null
         return (
             <div className="cart">
                 <img src={img} alt="Cart"/>
-                <span className="header-navbar-controls__item-counter">2</span>
-                <CartModal/>
+                <span className="header-navbar-controls__item-counter">{this.fullCount(items)}</span>
+                {headerCounter}
+                <CartModal
+                items={items}
+                onAddItem={this.addItemHandle}
+                onRemoveItem={this.removeItemHandle}
+                FullCount={this.fullCount}
+                FullSum={this.fullSum}
+                />
             </div>
         );
     }
